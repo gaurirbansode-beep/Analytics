97bf1b9cb4f05265bf6757bc85366c50084b312f
gaurirbansode-beep <gauri.r.bansode@gmail.com>
2026-03-12 11:07:55 +0530

    Add or update file: Output.py

--- Input/Output.py
+++ Input/Output.py
@@ -52,7 +52,91 @@ print(f"databricks_host:{databricks_host}")
 STATE_STARTED = "started"
 STATE_FINISHED = "finished"
 STATE_ERROR = "error"
-STATE_SKIPPED = "skipped"
+
+# COMMAND ----------
+
+class STSSession:
+    """
+    Class to init a sts session for the given role.
+    How to use:
+      # from lib.sts_session import STSSession
+
+      sts_session = STSSession(arn=<ASSUME_ROLE_ARN>,
+                          session_name=<SESSION_NAME>,
+                          duration=<OPTIONAL_SESSION_DURATION_IN_SECONDS>,
+                          region=<OPTIONAL_AWS_REGION>)
+    """
+
+    def __init__(
+        self, arn, session_name="sts_session", duration=3600, region="us-west-2"
+    ):
+        sts_connection = boto3.client("sts", region)
+        assume_role_object = sts_connection.assume_role(
+            RoleArn=arn, RoleSessionName=session_name, DurationSeconds=duration
+        )
+        self.credentials = assume_role_object["Credentials"]
+
+        self.sts_session = boto3.Session(
+            aws_access_key_id=self.credentials["AccessKeyId"],
+            aws_secret_access_key=self.credentials["SecretAccessKey"],
+            aws_session_token=self.credentials["SessionToken"],
+            region_name=region,
+        )
+
+# COMMAND ----------
+
+class AWSResource:
+    """
+    Class to create objects related to particular services of AWS.
+    How to use:
+        resource = AWSResource(session=<session_name>)
+    """
+
+    def __init__(self, session=boto3.session.Session()):
+        self.s3 = self.get_s3_bucket_object(session)
+
+    def get_s3_bucket_object(self, session):
+        return session.client("s3")
+
+    def refresh_s3_bucket_object(self, session):
+        self.s3 = session.client("s3")
+
+# COMMAND ----------
+
+def get_secret(secret_name, region_name="us-west-2", session=boto3.session.Session()):
+    """
+    Method to get secrets irrespective of session type. Please pass a STSSession if need to read secrets using assume-role.
+    How to use:
+        # Fetch secrets without assume role
+        secrets = get_secret(
+        secret_name=<SECRETS_NAME>,
+        region_name=<OPTIONAL_AWS_REGION>)
+
+        # Fetch secrets with assume role
+        secrets = get_secret(
+        secret_name=<SECRETS_NAME>,
+        region_name=<OPTIONAL_AWS_REGION>,
+        session=sts_session)     # code to initialize STSSession is defined above
+    """
+
+    client = session.client(
+        service_name="secretsmanager",
+        region_name=region_name,
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        raise e
+
+    else:
+        # Secrets Manager decrypts the secret value using the associated KMS CMK
+        # Depending on whether the secret was a string or binary, only one of these fields will be populated
+        if "SecretString" in get_secret_value_response:
+            secret_json = get_secret_value_response["SecretString"]
+            return json.loads(secret_json)
+        else:
+            return get_secret_value_response["SecretBinary"]
 
 # COMMAND ----------
 
@@ -79,13 +163,12 @@ except:
     log_data["job-name"] = f"notebook:{job_name}"
     source_type = "spark-notebook"
     source_name = job_name
-
 log_data["job-run-time"] = job_time
 print(log_data)
 
 # COMMAND ----------
 
-# Splunk logger migration: Commented Splunk blocks and replaced logger initialization
+# --- Splunk logger migration ---
 #splunk_secret = get_secret(splunk_secret_name)
 #logger = SplunkLogger(
 #    token=splunk_secret["token"],
@@ -96,8 +179,7 @@ print(log_data)
 #        "host": databricks_host,
 #    },
 #)
-
-logger = DatabricksLogger(
+logger = SplunkLogger(
     token="",
     index="",
     meta_data={
@@ -106,8 +188,6 @@ logger = DatabricksLogger(
         "host": databricks_host,
     },
 )
-
-
 def __get_event(log_level, msg, data={}):
     event = {"level": log_level, "message": msg}
     if isinstance(data, dict):
@@ -116,27 +196,16 @@ def __get_event(log_level, msg, data={}):
         event["data"] = data
     event.update(log_data)
     return json.dumps(event)
-
-
 def debug(msg: object, data: object = {}):
     logger.log_event(__get_event("DEBUG", msg, data))
-
-
 def info(msg: object, data: object = {}):
     logger.log_event(__get_event("INFO", msg, data))
-
-
 def warn(msg: object, data: object = {}):
     logger.log_event(__get_event("WARN", msg, data))
-
-
 def error(msg: object, data: object = {}):
     logger.log_event(__get_event("ERROR", msg, data))
-
-
 def fatal(msg: object, data: object = {}):
     logger.log_event(__get_event("FATAL", msg, data))
-
 print(__get_event("INFO", f"databricks logger initialized for {env} env"))
 info(f"databricks logger initialized for {env} env")
 logger.flush()
@@ -153,12 +222,8 @@ De-psedonymize: Use the decrypt udf
 """
 
 pseudonym_secrets = get_secret(f"{env}/k8s/p2retargeting/pseudonymize")
-
-
 def get_pseudonym_secret(key_type):
     return bytes(pseudonym_secrets[key_type], "utf-8")
-
-
 @udf
 def encrypt(key_type, text):
     if text is None:
@@ -179,8 +244,6 @@ def encrypt(key_type, text):
     except ValueError:
         warn("Error trying to encrypt")
         return None
-
-
 @udf
 def decrypt(key_type, cipher_text):
     if cipher_text is None:
@@ -193,8 +256,6 @@ def decrypt(key_type, cipher_text):
     except:
         warn("Error trying to decrypt")
         return None
-
-
 def pseudonymize(df, col_map):
     out_df = df
     for field, fieldtype in col_map.items():
@@ -205,8 +266,6 @@ def pseudonymize(df, col_map):
 
 class SourceEmptyException(Exception):
     pass
-
-
 def logging_wrapper(task, error_msg):
     def inner(func):
         def wrapper(*args, **kwargs):
@@ -251,20 +310,128 @@ def logging_wrapper(task, error_msg):
                     },
                 )
                 raise
-
         return wrapper
-
     return inner
 
 # COMMAND ----------
 
-# ... (rest of the original code unchanged, except Splunk logger blocks are commented and Databricks logger is used)
+def get_parquet_data(
+    source: str, partition_string: str = "", retain_partition_columns: bool = "False"
+) -> DataFrame:
+    if retain_partition_columns:
+        df = spark.read.option("basePath", source).parquet(source + partition_string)
+    else:
+        df = spark.read.option("mergeSchema", "true").parquet(source)
+    return df
+def get_delta_data(source: str) -> DataFrame:
+    return spark.read.format("delta").load(source)
+def get_unity_data(unity_path: str) -> DataFrame:
+    return spark.read.table(unity_path)
+def get_csv_data(source: str, separator: str = "|") -> DataFrame:
+    return (
+        spark.read.format("csv")
+        .option("header", "true")
+        .option("sep", separator)
+        .load(source)
+    )
+def get_decrypted_data_from_gpg(source, secret_name):
+    secret = get_secret(secret_name)
+    gpg = gnupg.GPG(gpgbinary="/usr/bin/gpg")
+    gpg.import_keys(key_data=secret["private_key"], passphrase=secret["passphrase"])
+    with s_open(source, mode="rb") as file:
+        decrypted_data = gpg.decrypt_file(file, passphrase=secret["passphrase"])
+    return decrypted_data
+def get_redshift_data(redshift_constants: dict, create_session: bool) -> DataFrame:
+    if create_session == True:
+        sess = STSSession(
+            arn=redshift_constants["CUMULUS_ARN"],
+            session_name=redshift_constants["ARN_ROLE_SESSION_NAME"],
+        )
+        secret = get_secret(
+            redshift_constants["REDSHIFT_SECRET_ID"], session=sess.sts_session
+        )
+    else:
+        secret = get_secret(redshift_constants["REDSHIFT_SECRET_ID"])
+    username = secret["username"]
+    password = secret["password"]
+    jdbc_connection = f"jdbc:redshift://{redshift_constants['REDSHIFT_HOST']}:{redshift_constants['PORT']}/{redshift_constants['DBNAME']}?ssl=true&sslfactory=com.amazon.redshift.ssl.NonValidatingFactory&user={username}&password={password}"
+    df = (
+        spark.read.format("com.databricks.spark.redshift")
+        .option("url", jdbc_connection)
+        .option("query", redshift_constants["QUERY"])
+        .option("tempdir", redshift_constants["TEMP_S3_DIR"])
+        .option("forward_spark_s3_credentials", True)
+        .load()
+    )
+    return df
+def log_and_load_data(source_info: dict, log_data: dict) -> DataFrame:
+    try:
+        info(
+            f"Starting {log_data['task']}",
+            data={
+                "task": log_data["task"],
+                "state": STATE_STARTED,
+            },
+        )
+        if source_info["format"] == "parquet":
+            df = get_parquet_data(
+                source_info["path"],
+                source_info.get("partition_string"),
+                source_info.get("retain_partition_columns"),
+            )
+        elif source_info["format"] == "delta":
+            df = get_delta_data(source_info["path"])
+        elif source_info["format"] == "csv":
+            df = get_csv_data(source_info["path"], source_info["separator"])
+        elif source_info["format"] == "unity":
+            df = get_unity_data(source_info["path"])
+        elif source_info["format"] == "redshift":
+            df = get_redshift_data(
+                source_info["constants"], source_info["create_session"]
+            )
+        elif source_info["format"] == "metastore":
+            if source_info["using_sql"] == True:
+                df = spark.sql(source_info["metastore_query"])
+            else:
+                df = spark.table(source_info["database"] + "." + source_info["table"])
+        else:
+            df = spark.read.format(source_info["format"]).load(source_info["path"])
+        info(
+            f"Finished {log_data['task']}",
+            data={
+                "task": log_data["task"],
+                "state": STATE_FINISHED,
+                "rows": df.count(),
+            },
+        )
+        return df
+    except AnalysisException as e:
+        error(
+            log_data["error_msg"],
+            data={
+                "task": log_data["task"],
+                "dump": str(e),
+                "state": STATE_ERROR,
+            },
+        )
+        if str(e).startswith("Path does not exist:"):
+            raise SourceEmptyException()
+        else:
+            raise e
+    except Exception as e:
+        error(
+            log_data["error_msg"],
+            data={
+                "task": log_data["task"],
+                "dump": str(e),
+                "state": STATE_ERROR,
+            },
+        )
+        raise e
 
 # COMMAND ----------
-
 # DBTITLE 1,Logger Flush on Exit
 import atexit
-
 def flush_logger_on_exit():
     """Ensure logger flushes remaining events before job ends"""
     try:
@@ -277,9 +444,6 @@ def flush_logger_on_exit():
             print("No remaining events to flush")
     except Exception as e:
         print(f"✗ Error flushing logger: {e}")
-
-# Register cleanup function
 atexit.register(flush_logger_on_exit)
-
-info(f"Clean room commons initialize for {env} env")
+info(f"databricks logger initialized for {env} env")
 logger.flush()
